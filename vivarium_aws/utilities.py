e5189cc97ba7ec78b7851b924395cd504493ccaf
halfhorst <chorst22@gmail.com>
2020-04-08 16:31:45 -0700

    add utility to get default region

--- vivarium_aws/utilities.py
+++ vivarium_aws/utilities.py
@@ -1,8 +1,15 @@
 import shutil
 
+import boto3
+
 
 def ensure_command_exists(command: str):
     """Raise a RuntimeError if `command` is not executable on the system."""
     if shutil.which(command) is None:
         raise RuntimeError(f"The command `{command}` must be installed on your "
                           "machine. Please consult the vaws requirements.")
+
+
+def get_default_region():
+    session = boto3.session.Session()
+    return session.region_name
